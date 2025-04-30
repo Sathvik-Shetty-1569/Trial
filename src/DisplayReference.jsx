82bd85cf8213f1374715a70dc80b4a71aec337cd
Sathvik-Shetty-1569 <sathvikshetty189@gmail.com>
2025-04-30 18:25:02 +0530

    Add more images function implemented

--- src/DisplayReference.jsx
+++ src/DisplayReference.jsx
@@ -1,16 +1,41 @@
-import { FlatList, Image, StyleSheet, Text, View, Dimensions } from 'react-native'
-import React from 'react'
+import { FlatList, Image, StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native'
+import React ,{useState} from 'react'
+import { launchImageLibrary } from 'react-native-image-picker';
+
 
 const DisplayReference = ({route, navigation}) => {
-const {images} = route.params;
-console.log(images);
+  const { images: initialImages = [] } = route.params || {};
+  const [images, setImages] = useState(initialImages); // ✅ Store in useStateconsole.log(images);
 const { width, height } = Dimensions.get('window');
 
+const Addmorefiles = async() => {
+  try{
+    const result = await launchImageLibrary({
+      mediaType: 'photo',
+      selectionLimit: 10,
+      includeBase64: true,
+    })
+
+    if(result.assets){
+      const newimages = result.assets.map(item => ({
+        uri: item.uri,
+        base64: item.base64,
+        type: item.type || 'image/jpeg',
+        name: item.fileName || `image_${Date.now()}`,
+      }))
+      setImages([...images,...newimages])
+    }
+
+  }
+  catch(error){
+    console.log(error);
+  }
+}
   return (
     <View style={{flex: 1}}>
       <FlatList
         data={images}
-        keyExtractor={(item, index) => index.toString()}
+        keyExtractor={item => item.uri}
         renderItem={({ item }) => (
           <Image
             source={{ uri: item.uri }}
@@ -22,11 +47,43 @@ const { width, height } = Dimensions.get('window');
         showsVerticalScrollIndicator={true}
         snapToInterval={height}
         decelerationRate="fast"
-    ></FlatList>
+    />
+    <View style={styles.buttonContainer}>
+    <TouchableOpacity
+    style={[styles.button, styles.uploadButton]}
+    onPress={Addmorefiles}
+    >
+    <Text style={styles.buttonText}>Add More Files</Text>
+    </TouchableOpacity>
+    </View>
         </View>
   )
 }
 
 export default DisplayReference
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button:{
+    backgroundColor: '#6200ee',
+    padding: 15,
+    borderRadius: 10,
+    elevation: 10,
+    alignItems: 'center',
+  }, 
+  buttonContainer:{
+    position: 'absolute',
+    bottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  uploadButton:{
+    backgroundColor: '#03dac6',
+
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+})
