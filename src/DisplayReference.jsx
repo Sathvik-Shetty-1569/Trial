13d2fb93d7c12e19c56cdd96606aef43affc10b0
Sathvik-Shetty-1569 <sathvikshetty189@gmail.com>
2025-05-01 12:50:43 +0530

    Images uploaded

--- src/DisplayReference.jsx
+++ src/DisplayReference.jsx
@@ -1,14 +1,42 @@
-import { FlatList, Image, StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View, Dimensions, TouchableOpacity, Alert, ActivityIndicator } from 'react-native'
 import React ,{useState} from 'react'
 import { launchImageLibrary } from 'react-native-image-picker';
+import axios from 'axios';
 import color from '../android/config/color';
+import { FLASK_SERVER_URL } from '@env';
 
 
 const DisplayReference = ({route, navigation}) => {
   const { images: initialImages = [] } = route.params || {};
   const [images, setImages] = useState(initialImages); // ✅ Store in useStateconsole.log(images);
 const { width, height } = Dimensions.get('window');
+const [isloading, setIsloading] = useState(false);
 
+const uploadFiles = async() => {
+  if(images.length == 0){
+    Alert.alert('No images selected');
+    return;
+  }
+  setIsloading(true);
+  try{
+    const uploadableImages = images.filter(img => img.base64);
+    console.log('Uploading these images:', uploadableImages);
+    const response = await axios.post(`http://192.168.1.205:5000/upload`, {
+      images: uploadableImages.map(img => `data:${img.type};base64,${img.base64}`)
+    }); 
+  
+    console.log('Server response:', response.data);
+
+   
+  }
+  catch(error){
+    console.log(error);
+  }
+  finally{
+    setIsloading(false);
+  }
+
+}
 const Addmorefiles = async() => {
   try{
     const result = await launchImageLibrary({
@@ -55,6 +83,18 @@ const Addmorefiles = async() => {
     onPress={Addmorefiles}
     >
     <Text style={styles.buttonText}>Add More Files</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity
+    style={[styles.button, styles.uploadButton]}
+    onPress={uploadFiles}
+    disabled={isloading}
+    >
+      {isloading ? (
+        <ActivityIndicator color="#fff" />
+      ): (
+    <Text style={styles.buttonText}>Upload Files</Text> )
+      }
     </TouchableOpacity>
     </View>
         </View>
